a4477e4ceb177b1c5175b2aad864301060345185
cauyifan <caiyf@yibanquan.com.cn>
2022-02-10 10:49:17 +0800

    新增switch

--- src/views/baseSetting/company.vue
+++ src/views/baseSetting/company.vue
@@ -1,21 +1,22 @@
 <template>
   <div>
-    <el-table
-      :data="tableData"
-      style="width: 100%">
-      <el-table-column
-        prop="date"
-        label="日期"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="name"
-        label="姓名"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="地址">
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="date" label="日期" width="180"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+      <el-table-column prop="address" label="地址"> </el-table-column>
+      <el-table-column label="状态">
+        <template slot-scope="scope">
+          <el-switch
+            v-model="scope.row.status"
+            active-color="#36c6d3"
+            active-text="启用"
+            inactive-text="关闭"
+            :active-value="true"
+            :inactive-value="false"
+            @change="active_text($event, scope.row)"
+          >
+          </el-switch>
+        </template>
       </el-table-column>
     </el-table>
   </div>
@@ -23,30 +24,43 @@
 
 <script>
 export default {
-   data() {
-        return {
-          tableData: [{
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄'
-          }, {
-            date: '2016-05-04',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1517 弄'
-          }, {
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄'
-          }, {
-            date: '2016-05-03',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1516 弄'
-          }]
-        }
-      }
-}
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+          status: true,
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄",
+          status: true,
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+          status: false,
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄",
+          status: true,
+        },
+      ],
+    };
+  },
+  methods: {
+    active_text(value, row) {
+      console.log(value, row);
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
